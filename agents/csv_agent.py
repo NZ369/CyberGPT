260c82b50284c5dbaff9b1a98748eadf21f477e3
LOFTuser <loftuser@LOFTadmins-MacBook-Air.local>
2023-07-06 17:15:25 -0400

    Changing to download from amazon s3 bucket.

--- agents/csv_agent.py
+++ agents/csv_agent.py
@@ -10,35 +10,37 @@ llm = create_llm()
 print("Creating CSV Agent.")
 combined_data = os.path.join("data", "combined.csv")
 
-mitre_dir = "../data"
-mitre_data = [os.path.join(mitre_dir, fn) for fn in ["software.csv", "groups.csv", "mitigations.csv"]]
-if not os.path.exists(combined_data):
-    combined_df = None
-    keys = ['TID', 'Technique Name']
-    for fp in mitre_data:
-        next_df = pd.read_csv(fp)
-        if combined_df is not None:
-            combined_df = combined_df.merge(
-                next_df,
-                on=['TID', 'Technique Name'],
-                how='outer'
-            )
-        else:
-            combined_df = next_df
+# mitre_dir = "../data"
+# mitre_data = [os.path.join(mitre_dir, fn) for fn in ["software.csv", "groups.csv", "mitigations.csv"]]
+# if not os.path.exists(combined_data):
+#     combined_df = None
+#     keys = ['TID', 'Technique Name']
+#     for fp in mitre_data:
+#         next_df = pd.read_csv(fp)
+#         if combined_df is not None:
+#             combined_df = combined_df.merge(
+#                 next_df,
+#                 on=['TID', 'Technique Name'],
+#                 how='outer'
+#             )
+#         else:
+#             combined_df = next_df
     
-    combined_df.to_csv(combined_data)
+#     combined_df.to_csv(combined_data)
 
 
 # Waiting on Secret Key info for Amazon.
-# def download_files(bucket_name="team5.2-mitre", data_dir="../data", files=[]):
-#     s3_client = boto3.client('s3')
-#     os.makedirs(data_dir, exist_ok=True)
-#     for fn in files:
-#         fp = os.path.join(data_dir, fn)
-#         s3_client.download_file(bucket_name, fn, fp)
+def download_files(bucket_name="team5.2-mitre", data_dir="data", files=[]):
+    s3_client = boto3.client('s3')
+    os.makedirs(data_dir, exist_ok=True)
+    for fn in files:
+        fp = os.path.join(data_dir, fn)
+        if not os.path.exists(fp):
+            print(f"Downloading {fn}")
+            s3_client.download_file(bucket_name, fn, fp)
 
-# download_files(files=["combined_data.csv"])
-# combined_data = os.path.join("../data", "combined_data.csv")
+download_files(files=["combined.csv"])
+
 
 mitre_csv_agent = create_csv_agent(
     llm,
