24120852c4b950c1675566d0793308ab9d838a09
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-07-07 18:11:02 -0400

    Implemented Kendra DB
     - Commented out other tools
     - Working requests

--- tools/get_tools.py
+++ tools/get_tools.py
@@ -3,13 +3,10 @@ from llms.azure_llms import create_llm
 from tools.prebuilt_tools import python_tool, wikipedia_tool, duckduckgo_tool
 from tools.qa_tools import qa_retrieval_tool
 from tools.borealis_tools import borealis_tool
-<<<<<<< HEAD
 from tools.opencti_tools import openCTI_tool
 from tools.shodan_tools import shodan_ip_lookup_tool
 from tools.ipapi_tools import ipapi_tool
-=======
-from tools.kendra_tool import kendra_retrieval_tool;
->>>>>>> 72be8d5 (Implemented kendra tool)
+from tools.kendra.tool import kendra_retrieval_tool;
 
 tool_llm = create_llm()
 tools=[]
