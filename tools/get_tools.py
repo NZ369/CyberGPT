72be8d592dc2528de811d2218c68526f84cbbb28
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-07-07 12:02:38 -0400

    Implemented kendra tool
    - doesn't work

--- tools/get_tools.py
+++ tools/get_tools.py
@@ -3,6 +3,7 @@ from llms.azure_llms import create_llm
 from tools.prebuilt_tools import python_tool, wikipedia_tool, duckduckgo_tool
 from tools.qa_tools import qa_retrieval_tool
 from tools.borealis_tools import borealis_tool
+from tools.kendra_tool import kendra_retrieval_tool;
 
 tool_llm = create_llm()
 tools=[]
@@ -12,3 +13,4 @@ tools.append(wikipedia_tool)
 tools.append(duckduckgo_tool)
 tools.append(qa_retrieval_tool)
 tools.append(borealis_tool)
+tools.append(kendra_retrieval_tool)
