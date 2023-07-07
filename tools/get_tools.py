bb826c505d30b0aaba0b56ad7aab2b9461248496
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-07-07 18:11:02 -0400

    Implemented Kendra DB
     - Commented out other tools
     - Working requests

--- tools/get_tools.py
+++ tools/get_tools.py
@@ -3,7 +3,7 @@ from llms.azure_llms import create_llm
 from tools.prebuilt_tools import python_tool, wikipedia_tool, duckduckgo_tool
 from tools.qa_tools import qa_retrieval_tool
 from tools.borealis_tools import borealis_tool
-from tools.kendra_tool import kendra_retrieval_tool;
+from tools.kendra.tool import kendra_retrieval_tool;
 
 tool_llm = create_llm()
 tools=[]
