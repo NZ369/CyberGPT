99bc956a02a4056df8f05650f488d23b77f2d908
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-07-07 12:02:38 -0400

    Implemented kendra tool
    - doesn't work

--- tools/get_tools.py
+++ tools/get_tools.py
@@ -3,9 +3,13 @@ from llms.azure_llms import create_llm
 from tools.prebuilt_tools import python_tool, wikipedia_tool, duckduckgo_tool
 from tools.qa_tools import qa_retrieval_tool
 from tools.borealis_tools import borealis_tool
+<<<<<<< HEAD
 from tools.opencti_tools import openCTI_tool
 from tools.shodan_tools import shodan_ip_lookup_tool
 from tools.ipapi_tools import ipapi_tool
+=======
+from tools.kendra_tool import kendra_retrieval_tool;
+>>>>>>> 72be8d5 (Implemented kendra tool)
 
 tool_llm = create_llm()
 tools=[]
@@ -18,3 +22,4 @@ tools.append(borealis_tool)
 tools.append(openCTI_tool)
 tools.append(shodan_ip_lookup_tool)
 tools.append(ipapi_tool)
+tools.append(kendra_retrieval_tool)
