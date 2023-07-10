bac390a3af021638add9b4427101175e74e2130b
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-07-10 15:05:14 -0400

    Update retriever.py

--- tools/kendra/retriever.py
+++ tools/kendra/retriever.py
@@ -43,29 +43,7 @@ class KendraRetriever(BaseRetriever):
     def _get_relevant_documents(
         self, query: str, *, run_manager: CallbackManagerForRetrieverRun
     ) -> List[Document]:
-        # request
-        response = requests.get(
-            'https://nc6toszo09.execute-api.ca-central-1.amazonaws.com/dev',
-            headers= {"content-type":"application/json"},
-            data = json.dumps({"query":query})
-        )
-        
-        #parse json
-        print(response.text)
-
-        response = json.loads(response.text)
-
-        # map to document
-        documents = list(
-            map(
-                lambda doc: Document(page_content = doc[1], metadata={'source': doc[0]}),
-                response
-            )
-        )
-
-        print(documents);
-
-        return documents
+        return get_relevant_documents(query)
     async def _aget_relevant_documents(
         self, query: str, *, run_manager: AsyncCallbackManagerForRetrieverRun
     ) -> List[Document]:
