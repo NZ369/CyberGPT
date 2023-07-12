296a2c12f14de063b5e55e307985367e4489a1ce
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-07-12 11:53:49 -0400

    Update profile_form.py

--- tools/CyberStance/profile_form.py
+++ tools/CyberStance/profile_form.py
@@ -23,20 +23,22 @@ def prompt_llm(query:str) -> str:
 def options_input_extractor(user_input: str, targets: List[str]) -> Tuple[bool, str]:
     result = prompt_llm(f"Convert query into one of the following N\A,{','.join(targets)}. query: {user_input}")
 '''
-form = Form([
-    Field("What is the size of your organization? (How many employees)",
-          lambda user_input: (False, "No numbers detected") if len(nums := re.findall(r'\d+', user_input)) != 1 else (True, "Small") if int(nums[0]) < 50 else (True, "Medium") if int(nums[0]) < 500 else (True, "Large")
-    ),
-    Field("Do you have a dedicated cyber security team or personnel? (yes or no)",
-          lambda user_input: (False, "Answer must be yes or no") if len(data := re.findall(r'(yes|no)', user_input.lower())) != 1 else (True, data[0].capitalize())
-    ),
-    Field("What is their IT/Computer expertise level? Please use one of the following options:",
-        lambda user_input: (False, "Invalid option") if len(data := re.findall(r'(Little to no experience|Moderate experience|High experience)', user_input)) != 1 else (True, data[0])
-    ),
-    Field("What industry sector does your organization fall under? Please use one of the following options:",
-        lambda user_input: (False, "Invalid option") if len(data := re.findall(r'(Consumer Products|Manufacturing|Service Providers|Technologies)', user_input)) != 1 else (True, data[0])
-    ),
-    Field("Does your organization follow any industry standards or frameworks? Select all that apply:",
-        lambda user_input: (False, "Invalid option") if len(data := re.findall(r'(ISO|NIST|ISM|ITSG|CIS|OTHER)', user_input.upper())) == 0 else (True, data)
-    )
-])
+
+def generate_new_profile_form():
+    return Form([
+        Field("What is the size of your organization? (How many employees)",
+            lambda user_input: (False, "No numbers detected") if len(nums := re.findall(r'\d+', user_input)) != 1 else (True, "Small") if int(nums[0]) < 50 else (True, "Medium") if int(nums[0]) < 500 else (True, "Large")
+        ),
+        Field("Do you have a dedicated cyber security team or personnel? (yes or no)",
+            lambda user_input: (False, "Answer must be yes or no") if len(data := re.findall(r'(yes|no)', user_input.lower())) != 1 else (True, data[0].capitalize())
+        ),
+        Field("What is their IT/Computer expertise level? Please use one of the following options:",
+            lambda user_input: (False, "Invalid option") if len(data := re.findall(r'(Little to no experience|Moderate experience|High experience)', user_input)) != 1 else (True, data[0])
+        ),
+        Field("What industry sector does your organization fall under? Please use one of the following options:",
+            lambda user_input: (False, "Invalid option") if len(data := re.findall(r'(Consumer Products|Manufacturing|Service Providers|Technologies)', user_input)) != 1 else (True, data[0])
+        ),
+        Field("Does your organization follow any industry standards or frameworks? Select all that apply:",
+            lambda user_input: (False, "Invalid option") if len(data := re.findall(r'(ISO|NIST|ISM|ITSG|CIS|OTHER)', user_input.upper())) == 0 else (True, data)
+        )
+    ])
