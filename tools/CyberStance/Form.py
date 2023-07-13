7dabec139445e918943fd857b9efbaf8d4acca88
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-07-12 20:16:32 -0400

    Updated field & form datastructure
    - Added __repr__ function
    - Added first_unaswered question method to form.py

--- tools/CyberStance/Form.py
+++ tools/CyberStance/Form.py
@@ -1,14 +1,28 @@
 from tools.CyberStance.field import Field
 from typing import List, Dict
 from json import dump
+import streamlit as st
 
 class Form:
     def __init__(self, fields: List[Field]) -> None:
         self.fields = fields
+
+    def first_unanswered(self) -> Field | None:
+        for f in self.fields:
+            if not f.is_valid():
+                return f
+        return None
+
     def completed(self) -> bool:
         return all(f.is_valid() for f in self.fields)
 
     def to_json(self) -> str:
         return dump(self.to_dict())
+
     def to_dict(self) -> Dict[str, str]:
         return {field.question: field.value for field in self.fields}
+
+    def __repr__(self) -> str:
+        # return a string representation of the Form instance
+        # that shows the list of fields
+        return f"Form(fields={self.fields!r})"
--- tools/CyberStance/field.py
+++ tools/CyberStance/field.py
@@ -3,16 +3,12 @@ from typing import Callable, Tuple
 class Field:
     def __init__(self, question: str, input_parser: Callable[[str], Tuple[bool, str]], value: str | None = None):
         self.question: str = question
-
-        self._input_parser: Callable[[str], Tuple[bool, str]] = input_parser
-        
+        self._input_parser = input_parser
+            
         self.value: str | None = value
 
     def is_valid(self) -> bool:
-        if self.value == None:
-            return False
-        cond, _ = self._input_parser(self.value)
-        return cond
+        return self.value is not None
 
     def input_parser(self, user_input: str) -> Tuple[bool, str]:
         cond, result = self._input_parser(user_input)
@@ -21,3 +17,7 @@ class Field:
             self.value = result
         
         return cond, result
+    def __repr__(self) -> str:
+        # return a string representation of the Field instance
+        # that shows the question, the value, and the input_parser function
+        return f"Field(question={self.question!r}, value={self.value!r}, input_parser={self._input_parser.__name__!r})"
