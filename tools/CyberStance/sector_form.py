a74313a2822667631e8fce6150fcc83290c8afe7
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-07-13 09:39:14 -0400

    Update sector_form.py
    - Created email sector form

--- tools/CyberStance/sector_form.py
+++ tools/CyberStance/sector_form.py
@@ -8,5 +8,22 @@ def generate_form_selection_field():
         lambda user_input: (False, "Invalid option") if len(result := re.findall(r'email', user_input.lower())) != 1 else (True, user_input.lower())
     )
 
+
+def generate_email_form():
+    forms = Form([
+        Field("Do you have SPF (Sender Policy Framework) enabled and configured? (yes or no)",
+            lambda user_input: (False, "Answer must be yes or no") if len(data := re.findall(r'(yes|no)', user_input.lower())) != 1 else (True, data)
+        ),
+        Field("Do you have DMARC (Domain Message Authentication Reporting & Conformance) enabled and configured? (yes or no)",
+            lambda user_input: (False, "Answer must be yes or no") if len(data := re.findall(r'(yes|no)', user_input.lower())) != 1 else (True, data)
+        ),
+        Field("Do you have DKIM (DomainKeys Identified Mail) enabled and configured? (yes or no)",
+            lambda user_input: (False, "Answer must be yes or no") if len(data := re.findall(r'(yes|no)', user_input.lower())) != 1 else (True, data)
+        )
+    ])
+
+    return forms
+
+generate_form = {"email": generate_email_form}
 # create field for different forms
 # create email sector form
